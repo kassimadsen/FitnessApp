9ff577a47257874a18a0d7aa6f627fae9fe26c5f
kassimadsen <madsenkassi@gmail.com>
2025-01-30 21:34:57 -0700

    started some css and pages

--- ActiveX/src/routes/+page.svelte
+++ ActiveX/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Hero from './Hero.svelte';
+</script>
+
+<Hero />
+<a class="cta" href="/programme">This week's programme</a>
+<a class="cta" href="/generate">Generate a programme</a>
+<a class="cta" href="/discord">join the community</a>
+
+<style lang="postcss">
+	.cta {
+		display: block;
+		background-color: black;
+		color: papayawhip;
+		padding: 0.5rem 1 rem;
+	}
+</style>
---
+++ ActiveX/src/routes/Hero.svelte
@@ -0,0 +1,15 @@
+<div class="stack">
+	<h1>ActiveX</h1>
+</div>
+
+<style lang="postcss">
+	.stack {
+		background-color: orange;
+		height: 50svh;
+	}
+
+	h1 {
+		font-size: 3rem;
+		place-self: center;
+	}
+</style>
