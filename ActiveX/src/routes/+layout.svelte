61fdf030e270b32164e19f1c562d25bb252a1b4d
kassimadsen <madsenkassi@gmail.com>
2025-02-03 20:53:16 -0700

    added toggle dark mode

--- ActiveX/src/routes/+layout.svelte
+++ ActiveX/src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import '../app.css';
+	import Button from '$lib/components/Button.svelte';
 	let {children} = $props();
 </script>
 
 <header><a href="/">ActiveX</a></header>
+<Button
+	onclick={() => {
+		window?.toggleTheme && window.toggleTheme();
+	}}>toggle theme</Button
+>
 
 {@render children()}
 <footer>Made with 🧡 by Kassi</footer>
